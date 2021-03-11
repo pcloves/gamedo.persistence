23d544a89ae9430f4f916012592233cda1f42f7f
zhengtongshan <pcloves@sina.com>
2021-03-11 20:27:05 +0800

    format the coed

--- src/main/java/org/gamedo/persistence/config/MongoConfiguration.java
+++ src/main/java/org/gamedo/persistence/config/MongoConfiguration.java
@@ -1,12 +1,10 @@
 package org.gamedo.persistence.config;
 
 import lombok.Getter;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.data.mongodb.MongoDatabaseFactory;
-import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.convert.*;
 import org.springframework.data.mongodb.core.mapping.MongoMappingContext;
 
@@ -14,7 +12,6 @@ import java.util.Arrays;
 import java.util.Collections;
 
 @Configuration
-@ConditionalOnBean(MongoTemplate.class)
 public class MongoConfiguration {
 
     @Getter
